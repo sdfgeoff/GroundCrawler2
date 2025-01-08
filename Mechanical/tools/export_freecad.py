9bcc749851da9ea51b4ff464e34e21abe8698ccc
Geoffrey Irons <sdfgeoff@gmail.com>
2025-01-08 23:00:11 +1300

    Undo!

--- Mechanical/tools/export_freecad.py
+++ Mechanical/tools/export_freecad.py
@@ -5,7 +5,7 @@ from tempfile import TemporaryDirectory
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
-FREECAD_BINARY = os.environ.get("FREECADCMD", "freecadcmd")
+FREECAD_BINARY = "freecadcmd"
 EXPORT_SINGLE_FILENAME = os.path.join(HERE, "./export_freecad_internal.py")
 
 # Tup doesn't allow leaving behind temporary files, so we need a temporary folder for our python code so that pycache files are cleaned up properly
